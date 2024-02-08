d2022b5dc463a6bc255a88a6c0eff15a816bf9ff
Rakotoarivo Loick <120171050+Avocadorable0@users.noreply.github.com>
2024-02-08 17:23:43 +0300

    change function

--- src/main/java/farm/demo/controller/ContollerMessagerie.java
+++ src/main/java/farm/demo/controller/ContollerMessagerie.java
@@ -13,7 +13,7 @@ import java.sql.Timestamp;
 public class ContollerMessagerie {
 
     @CrossOrigin(origins = "*")
-    @PostMapping(value = "/insertMessage", produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/insertmessage", produces = MediaType.APPLICATION_JSON_VALUE)
     public String insertMessage(@RequestParam String envoyeur, @RequestParam String receveur, @RequestParam String contenu)
     {
         try{
@@ -25,7 +25,6 @@ public class ContollerMessagerie {
             m.insertMessage(c,nM);
             return "{ \"success\": \"Insertion réussie\" }";
         }
-
         catch (Exception e) {
             e.printStackTrace();
             return "{ \"error\": \"Oups... Quelque chose s'est mal passé\" }";
